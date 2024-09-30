4ddf49cda7020c08ffb2591c66617524e919b7e4
Natalia Pochekutova <natalipochekutova51@gmail.com>
2024-10-01 00:29:46 +0800

    masks and utils logger

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,31 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
+                    filename='../logs/masks_log.log',
+                    filemode='w')
+
+mask_card_number_logger = logging.getLogger()
+mask_account_logger = logging.getLogger()
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты"""
+    mask_card_number_logger.info('Запуск маскировки номера карты')
     if card_number.isdigit() and len(card_number) == 16:
+        mask_card_number_logger.info('Маска номера карты создана')
         return f"{card_number[0: 4]} {card_number[4: 6]}{"*" * 2} {"*" * 4} {card_number[-4:]}"
     else:
+        mask_card_number_logger.info('Неверные данные')
         return "Неверные данные"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскировки номера счета"""
+    mask_account_logger.info('Запуск маскировки номера счета')
     if account_number.isdigit() and len(account_number) == 20:
+        mask_account_logger.info('Маска номера счета создана')
         return f"{"*" * 2}{account_number[-4::]}"
     else:
+        mask_account_logger.info('Неверные данные')
         return "Неверные данные"
-
-
-print(get_mask_card_number("2204528462589512"))
-print(get_mask_account("47896523789654258942"))
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,29 @@
 import json
+import logging
 from json import JSONDecodeError
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s",
+    filename="../logs/utils.log",
+    filemode="w",
+)
+
+get_financial_transactions_logger = logging.getLogger()
 
 
 def get_financial_transactions(path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    get_financial_transactions_logger.info("Начало работы функции")
     try:
         with open(path, encoding="utf-8") as file:
             try:
                 transactions = json.load(file)
+                get_financial_transactions_logger.info("Создан список словарей финансовых транзакций")
                 return transactions
             except JSONDecodeError:
+                get_financial_transactions_logger.error("Ошибка файла с транзакциями")
                 return []
     except FileNotFoundError:
+        get_financial_transactions_logger.error("Файл с транзакциями не найден")
         return []
